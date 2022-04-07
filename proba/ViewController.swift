4601f553eac1f75bdb010810cf02c1fe97d44f2d
Nadya Lizunova <bardeeva_nv@icloud.com>
2022-04-07 13:23:11 +0300

    First commit

---
+++ proba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proba
+//
+//  Created by Бардеева Надежда on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
